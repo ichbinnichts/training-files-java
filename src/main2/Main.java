2dbae782cea57a48a89a7543594c4f5c7c8f972f
Nathan Víctor de Faria <nathan.faria@inatel.br>
2022-08-20 13:42:34 -0300

    feat: add mkdir to file handler

--- src/main2/Main.java
+++ src/main2/Main.java
@@ -16,8 +16,24 @@ import projeto.Membro;
 public class Main {
     public static void main(String[] args){
         FileHandler file = new FileHandler();
-        file.createDir("Projetos/Test");
+        file.createDir("Projetos");
         Projeto projeto = new Projeto();
+        projeto.setName("Test2");
+        ArrayList<Object> membros = new ArrayList<Object>();
+        Membro membro1 = new Membro();
+        membro1.setFirstName("John");
+        membro1.setLastName("Smith");
+        Membro membro2 = new Membro();
+        membro2.setFirstName("Alex");
+        membro2.setLastName("Murray");
+        membros.add(membro1);
+        membros.add(membro2);
+        projeto.setMembros(membros);
+        ArrayList<Object> list = new ArrayList<Object>();
+        list.add(projeto);
+        String name = projeto.getName();
+        file.writer(list, ("Projetos/"+name+".txt"));
+        
         
     }
 }
